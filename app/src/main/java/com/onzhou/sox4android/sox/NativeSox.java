44ecdfce199b748c3fbd719a8ffa924cf83d34dc
byhook <byhook@163.com>
2018-12-11 22:57:16 +0800

    基于cmake链接sox静态库

---
+++ app/src/main/java/com/onzhou/sox4android/sox/NativeSox.java
@@ -0,0 +1,14 @@
+package com.onzhou.sox4android.sox;
+
+/**
+ * @anchor: andy
+ * @date: 18-12-11
+ */
+
+public class NativeSox {
+
+    static {
+        System.loadLibrary("native-sox");
+    }
+
+}
